8741ef8c04cd2ecec23c2045bb9fc97b7ecb61d9
geoquimica-gomez <geoquimica.gomez@gmail.com>
2024-08-08 11:34:25 -0400

    creando loginpage

--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -1,12 +1,43 @@
 
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 const LoginPage = () => {
     return (
-            <Container className="mt-4">
-                <h1>Probando LoginPage</h1>
-            </Container>
+        <Container
+            fluid
+            className="p-3"
+            style={{ maxWidth: '90%', backgroundColor: '#fc4b1a5b', margin: '10px auto', borderRadius: '10px',
+                borderTop: '10px solid #333',
+                borderBottom: '10px solid #333' }}
+        >
+            <Row>
+                <Col md={4} className="d-none d-md-block">
+                    <img
+                        src="https://png.pngtree.com/thumb_back/fw800/background/20240216/pngtree-pizza-menu-cover-background-image_15627187.jpg"
+                        alt="Decorative"
+                        style={{ width: '100%', height: 'auto' }}
+                    />
+                </Col>
+                <Col md={8}>
+                    <Form className='customForm'>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Register
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     );
-}
+};
 
 export default LoginPage;
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/desafio17Hito2/",
   plugins: [react()],
 })
